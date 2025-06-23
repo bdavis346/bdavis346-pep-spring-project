e30febd3869d43f4eab15f6748d8cc97f95a6ec0
B Davis <bdavis49382@gmail.com>
2025-06-23 21:24:03 +0000

    cleaned up todos and unused imports

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,12 +21,6 @@ import com.example.service.AccountService;
 import com.example.service.MessageService;
 import com.example.entity.*;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
- * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
- * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
 @RestController
 public class SocialMediaController {
     MessageService messageService;
@@ -70,7 +63,7 @@ public class SocialMediaController {
 
     @GetMapping("/accounts/{id}/messages")
     public List<Message> getAllMessagesForUser(@PathVariable int id) {
-        
+
         return messageService.getAllMessagesForUser(id);
     }
 
